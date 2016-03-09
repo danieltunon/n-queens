60579f9e3b7ae19005062a889757f1c3444a17a2
Danny Tunon <danieltunon@gmail.com>
2016-03-09 13:59:14 -0800

    Implement helper functions

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,31 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      // compute sum of values for row
+      var spotsFilled = this.rows()[rowIndex].reduce(function(sum, val) {
+        return sum += val;
+      }, 0);
+      // if sum is greater than 1 return true
+      if ( spotsFilled > 1 ) {
+        return true;
+      }
+      // else return false
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // loop through all board rows
+      var rows = this.rows();
+      for ( var i = 0; i < rows.length; i++ ) {
+        // check if current row for conflict using prev fn
+        if ( this.hasRowConflictAt(i) ) {
+          // if conflict return true
+          return true;
+        }
+      }
+      // return false
+      return false;
     },
 
 
@@ -94,12 +113,35 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      // loop through all rows
+      var rows = this.rows();
+      var spotsFilled = rows.reduce(function(sum, row) {
+        // for each row determine value at column index
+        // sum all values for that column as you loop through rows
+        return sum += row[colIndex];
+      }, 0);
+      // if sum is greater than 1 return true
+      if ( spotsFilled > 1 ) {
+        return true;
+      }
+      // else return false
+      return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      // get number of columns
+      var n = this.get('n');
+      // loop through all col indexes
+      for ( var i = 0; i < n; i++ ) {
+        // call prev fn for each index
+        // if any conflict return true
+        if ( this.hasColConflictAt(i) ) {
+          return true;
+        }
+      }
+      // else return false
+      return false;
     },
 
 
@@ -109,12 +151,42 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // copy index passed in and store in new variable
+      var index = majorDiagonalColumnIndexAtFirstRow;
+      // loop through each row
+      var rows = this.rows();
+      var spotsFilled = rows.reduce(function(sum, row) {
+        // check value at current value of index
+        // if it's undefined call it zero
+        // add the value to sum
+        // increment the index
+        return sum += ( row[index++] || 0 );
+      }, 0);
+      // return true if the sum is greater than 1
+      if ( spotsFilled > 1 ) {
+        return true;
+      }
+      // else return false
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      // loop each row
+      // check each col starting at -(n-1)
+      var rows = this.rows();
+      var col = -( this.get('n') - 1 );
+      rows.forEach(function() {
+        // check for diagonal conflict at that column index (prev fn)
+        if ( this.hasMajorDiagonalConflictAt(col) ) {
+          // if conflict found return true
+          return true;
+        }
+        // increment column
+        col++;
+      });
+      // return false
+      return false;
     },
 
 
@@ -124,12 +196,42 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // copy index passed in and store in new variable
+      var index = minorDiagonalColumnIndexAtFirstRow;
+      // loop through each row
+      var rows = this.rows();
+      var spotsFilled = rows.reduce(function(sum, row) {
+        // check value at current value of index
+        // if it's undefined call it zero (happens when index >= n)
+        // add the value to sum
+        // decrement the index
+        return sum += ( row[index--] || 0 );
+      }, 0);
+      // return true if the sum is greater than 1
+      if ( spotsFilled > 1 ) {
+        return true;
+      }
+      // else return false
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      // loop through each row
+      var rows = this.rows();
+      // check each column starting at 2(n-1)
+      var col = 2 * ( this.get('n') - 1 );
+      rows.forEach(function() {
+        // check for diagonal conflict at that column index (prev fn)
+        if ( this.hasMinorDiagonalConflictAt(col) ) {
+          // if conflict found return true
+          return true;
+        }
+        // else decrement column
+        col++;
+      });
+      // return false
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
